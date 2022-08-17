4d7f2c894b9a56e7f165af36030507412a1fcc38
Varun Thomas <varunthomas.mec@gmail.com>
2022-08-17 11:34:55 +0530

    Added bricks

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -14,16 +14,13 @@ public class Ball : MonoBehaviour
 	}
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
-		Debug.Log("Collison");
 		if(collision.gameObject.tag == "FallCheck")
 		{
-			Debug.Log("Restart");
 			GameManager.instance.Restart();
 		}
 		else if(collision.gameObject.tag == "Paddle")
 		{
-			GameManager.instance.ScoreUp();
-			Debug.Log("No restart");
+			GameManager.instance.ScoreUp(1);
 		}
 	}
     // Start is called before the first frame update
@@ -36,7 +33,6 @@ public class Ball : MonoBehaviour
     void Update()
     {
 		rb.velocity = bounceForce* (rb.velocity.normalized);
-		Debug.Log("Vel " + rb.velocity);
 	if (!gameStarted)
 	{
 		if(Input.anyKeyDown)
