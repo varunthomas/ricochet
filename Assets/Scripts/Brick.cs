65d3b66b79234e46307352bcadfa621e03864c66
Varun Thomas <varunthomas.mec@gmail.com>
2022-08-23 16:22:16 +0530

    Bricks move down

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -4,29 +4,39 @@ using UnityEngine;
 //.32
 public class Brick : MonoBehaviour
 {
+	float speed = 0.02f;
+	float height = 0.01f;
+	float startY;
 	private bool hasCollided = false;
     // Start is called before the first frame update
     void Start()
     {
-
+		startY = transform.position.y;
     }
 
     // Update is called once per frame
-	/*void LateUpdate()
+	void Update()
 	{
-		this.hasCollided = false;
-	}*/
+		var pos = transform.position;
+		var newY = startY - height*speed;//*Mathf.Sin(Time.time * speed);
+		startY = newY;
+		transform.position = new Vector3(pos.x, newY, pos.z);
+	}
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
-			if(collision.gameObject.tag == "Ball")
-			{
-				if(hasCollided == true){ return; }
-				hasCollided = true;
-				Debug.Log("collided brick");
-				GameManager.instance.ScoreUp(5);
-				Debug.Log("Destroy brick");
-				Destroy(gameObject);
-			}
+		if(collision.gameObject.tag == "Ball")
+		{
+			if(hasCollided == true){ return; }
+			hasCollided = true;
+			Debug.Log("collided brick");
+			GameManager.instance.ScoreUp(5);
+			Debug.Log("Destroy brick");
+			Destroy(gameObject);
+		}
+		else if(collision.gameObject.tag == "Paddle" || collision.gameObject.tag == "FallCheck")
+		{
+			GameManager.instance.Restart();
+		}
 
 	}
 }
