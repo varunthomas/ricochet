bdf9ed4705e0dbb16fc44bdd197334ba84965247
Varun Thomas <varunthomas.mec@gmail.com>
2022-08-25 18:43:54 +0530

    Added text between levels and reset position of paddle after level

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -7,6 +7,7 @@ public class Paddle : MonoBehaviour
 
 	Rigidbody2D rb;
 	public float moveSpeed;
+	Vector2 originalPos;
 	private void Awake()
 	{
 		rb = GetComponent<Rigidbody2D>();
@@ -14,21 +15,33 @@ public class Paddle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+		originalPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y);   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
     }
 	private void FixedUpdate()
 	{
+		/*if (GameManager.instance.isWin == true)
+		{
+			rb.constraints = RigidbodyConstraints2D.FreezeAll;
+		}
+		else
+		{
+			rb.constraints = RigidbodyConstraints2D.None;
+			rb.freezeRotation = true;
+			rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+		}*/
 		TouchMove();
+		
 	}
 	void TouchMove()
 	{
-		if(Input.GetMouseButton(0))
+		if(GameManager.instance.isWin == false && Input.GetMouseButton(0))
 		{
 			Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			if(touchPos.x < 0)
@@ -39,6 +52,11 @@ public class Paddle : MonoBehaviour
 			{
 				rb.velocity = Vector2.right*moveSpeed;
 			}
+		}
+		else if( GameManager.instance.isWin == true)
+		{
+			transform.position = originalPos;
+			rb.velocity = Vector2.zero;
 		}
 		else
 		{
